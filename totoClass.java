a89b0e1e1a0b65e5a6b09c1c4c10be70337a4211
Nacera123 <nace.slimane@gmail.com>
2022-03-21 15:07:59 +0100

    2eme commit

--- totoClass.java
+++ totoClass.java
@@ -1,2 +1,28 @@
 public class totoClass {
+    final static int price =150;
+    int state = 0;
+
+
+    void toto(int coin){
+
+        state =state + coin;
+
+        if (state>= price){
+
+            serve();
+            champ(amount state -150);
+            state =0;
+        }
+    }
+
+
+    void serve(){
+
+    }
+
+    /**
+     * test
+     */
+
+    void champ (int amount)
 }
